a8ba875ca17961a8deb967b242d6503121288291
Yashh Jaggi <yashhjaggi1998@gmail.com>
2024-05-27 22:20:04 -0700

    Fixing tag space issue

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -24,7 +24,7 @@ const PromptCard = (prop) => {
   };
 
   function transformString(inputString) {
-    return '#' + inputString.replace(/\s/g, '').replace(/^#?/, '');
+    return '#' + inputString.replace(/^#?/, '');
 }
 
   const handleCopy = () => {
